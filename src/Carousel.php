aed112964878d611279056b7c4c505322ff0e974
Alexander Kochetov <creocoder@gmail.com>
2015-04-12 22:39:33 +0300

    Updated dosamigos\gallery\Carousel

--- src/Carousel.php
+++ src/Carousel.php
@@ -1,9 +1,10 @@
 <?php
 /**
- * @copyright Copyright (c) 2013 2amigOS! Consulting Group LLC
- * @link http://2amigos.us
- * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @link https://github.com/2amigos/yii2-gallery-widget
+ * @copyright Copyright (c) 2013-2015 2amigOS! Consulting Group LLC
+ * @license http://opensource.org/licenses/BSD-3-Clause
  */
+
 namespace dosamigos\gallery;
 
 use yii\helpers\Html;
@@ -12,10 +13,7 @@ use yii\helpers\Json;
 /**
  * Carousel BlueImp Gallery Widget
  *
- * @author Antonio Ramirez <amigo.cobos@gmail.com>
- * @link http://www.ramirezcobos.com/
- * @link http://www.2amigos.us/
- * @package dosamigos\gallery
+ * @author Alexander Kochetov <creocoder@gmail.com>
  */
 class Carousel extends Gallery
 {
@@ -75,4 +73,4 @@ class Carousel extends Gallery
         $js = "blueimp.Gallery($items, $options);";
         $view->registerJs($js);
     }
-} 
+}
