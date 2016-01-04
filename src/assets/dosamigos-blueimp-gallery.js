178b9f71feef8638af977c19be50808402ca6518
Antonio Ramirez <amigo.cobos@gmail.com>
2016-01-04 06:58:47 +0100

    remove overriding  - thanks @firstsano

--- src/assets/dosamigos-blueimp-gallery.js
+++ src/assets/dosamigos-blueimp-gallery.js
@@ -8,8 +8,8 @@ if (typeof dosamigos == "undefined" || !dosamigos) {
 }
 dosamigos.gallery = (function($){
     var pub = {
-        registerLightBoxHandlers: function(selector, options) {
-            var options = options || {};
+        registerLightBoxHandlers: function(selector, opts) {
+            var options = opts || {};
             $(document).off('click.gallery', selector).on('click.gallery', selector, function() {
                 var links = $(this).parent().find('a.gallery-item');
                 options.index = $(this)[0];
